b1062fefd4902d6170ae01626c8b09964023315f
Dan Askew <askewholdings@gmail.com>
2023-11-18 09:21:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import openai
+import dotenv 
+
+# secrets for API keys
+env_path = os.path.join(os.path.dirname(__file__), '..', 'secrets', '.env')
+load_dotenv(env_path)
+
+OPENAI_KEY = os.getenv("OPENAI_API_KEY")
+
 
